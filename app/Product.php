e2dc9da9296ef46bc98187543ceeed3a552093cc
Raul Gomez <raul_nouni@hotmail.com>
2020-08-25 00:34:45 -0500

    Relationships with Eloquent

--- app/Product.php
+++ app/Product.php
@@ -8,4 +8,12 @@ class Product extends Model
 {
     // Como estamos usando asignacion masiva se agrega la propiedad guarded para que se puedan crear productos con cualquier atributo
     protected $guarded = [];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function createdBy() {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        factory(\App\User::class, 10)->create();
+        $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
     }
 }
